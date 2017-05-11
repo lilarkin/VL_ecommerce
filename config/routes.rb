7a43054b412b70576dc4dd87a0feb4501013de64
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-05-11 14:00:38 -0600

    fixes bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'index#home'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+
 end
