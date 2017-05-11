3214b4e51bf019ab038a056993aa044d9b677e09
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-05-11 15:32:36 -0600

    adss home controller, fixes routes, adds gems for better errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require materialze-sprockets
+//= require materialize-sprockets
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'index#home'
+ root 'home#index'
 
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions'
-  # }
+ devise_for :users, controllers: {
+   sessions: 'users/sessions',
+   registrations: 'users/registrations'
+ }
+
+  namespace :api do
+    get 'logged_in_user', to: 'users#logged_in_user'
+  end
+
+  # REQUIRED FOR REACT ROUTER - NO ROUTES BELOW THIS
+  get '*unmatched_route', to: 'home#index'
 
 end
